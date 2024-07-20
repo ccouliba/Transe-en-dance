9e0b229b4f8df41833e46c0ab4b3369cd9406395
gfritsch <gfritsch@student.42.fr>
2024-07-20 19:38:19 +0200

    debut d'integration de bootstrap dans le backend : ajout de deux lignes danss base.html pour charger bootstrap. menu ecrit.

--- backend/project/pong/static/pong/js/menu.js
+++ backend/project/pong/static/pong/js/menu.js
@@ -1,15 +1,47 @@
 function Menu() {
 	const isLoggedIn = localStorage.getItem('userToken') === 'true';
 	return `
-		<nav class="navbar">
+		<div class="menuStructureTop">
+		<nav class="navbar navbar-expand-lg navbar-custom menuStructureTop">
 			${isLoggedIn ? `
-				<a href="#home" onclick="changePage('#home'); return false;">Home</a>
-				<a href="#play" onclick="changePage('#play'); return false;">Play</a>
-				<a href="#tournament" onclick="changePage('#tournament'); return false;">Tournament</a>
-				<a href="#profile" onclick="changePage('#profile'); return false;">Profil</a>
-				<a href="#friends" onclick="changePage('#friends'); return false;">Friends</a>
-				<a href="#" onclick="logout(); return false;">Logout</a>
-			` : `
+\			<div class="container-fluid">
+				<a class="navbar-brand active" href="#home" onclick="changePage('#home'); return false;" id="navbarHomeLink"; >Home</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+					aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbar">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" aria-current="page" href="#play" id="navbarPlayLink" onclick="changePage('#play'); return false;">Play</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" aria-current="page" href="#tournament"
+								id="navbarTournamentLink" onclick="changePage('#tournament'); return false;">Tournament</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" aria-current="page" href="#profile"
+								id="navbarProfileLink" onclick="changePage('#profile'); return false;">Profile</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" aria-current="page" href="#friends"
+								onclick="changePage('#friends'); return false;" id="navbarFriendsLink">Friends</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" aria-current="page" href="#" onclick="logout(); return false;"
+								id="navbarLogoutLink">Log Out</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"
+								id="navbarDisabled">Disabled</a>
+							<!-- possibilite de rendre le bouton desactive selon l'etat du user (enrengistre ou pas, ect)-->
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+	` : `
 				
 			`}
 		</nav>
