811a3676cd4dc2a0925ebb8f44d70480054ff5a9
gfritsch <gfritsch@student.42.fr>
2024-07-20 18:08:33 +0200

    avec carrel, fixs du demarrage du projet

--- backend/project/pong/forms.py
+++ backend/project/pong/forms.py
@@ -1,28 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-<<<<<<< HEAD
-from django.conf import settings
-from django.utils.translation import gettext_lazy as _
-
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-        
-class SetLanguageForm(forms.Form):
-    language = forms.ChoiceField(
-        choices=[
-            ('en', _('English')),
-            ('fr', _('French')),
-            ('it', _('Italian')),
-            ('es', _('Spanish')),
-            ('de', _('Deutsch')),
-        ],
-        label=_('Select your language')
-    )
-    
-=======
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -75,4 +52,3 @@ class RegisterForm(UserCreationForm):
 #             raise forms.ValidationError('password must match')
 		
 		
->>>>>>> spa
