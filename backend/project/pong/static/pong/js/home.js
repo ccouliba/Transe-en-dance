d4355b98998463fa8bceb6e8c9d515273be197b6
jfremond <jfremond@student.42.fr>
2024-08-06 12:47:02 +0200

    undo stuff

--- backend/project/pong/static/pong/js/home.js
+++ backend/project/pong/static/pong/js/home.js
@@ -1,20 +1,20 @@
 
-// function Home() {
-// 	return `
-// 	<div>
-// 		<h1>Welcome home</h1>
-// 	</div>`;
-// }
+function Home() {
+	return `
+	<div>
+		<h1>Welcome home</h1>
+	</div>`;
+}
 
-loadTranslationScript(function() {
-    function Home() {
-        return `
-        <div>
-            <h1>${gettext("Welcome home")}</h1>
-        </div>`;
-    }
+// loadTranslationScript(function() {
+//     function Home() {
+//         return `
+//         <div>
+//             <h1>${gettext("Welcome home")}</h1>
+//         </div>`;
+//     }
 
-    document.addEventListener('DOMContentLoaded', function() {
-        document.body.innerHTML = Home();
-    });
-});
+//     document.addEventListener('DOMContentLoaded', function() {
+//         document.body.innerHTML = Home();
+//     });
+// });
