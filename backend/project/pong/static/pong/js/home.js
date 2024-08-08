dc031b6143536b52be819833649812097983214b
jfremond <jfremond@student.42.fr>
2024-08-08 15:55:48 +0200

    Home is done

--- backend/project/pong/static/pong/js/home.js
+++ backend/project/pong/static/pong/js/home.js
@@ -1,56 +1,19 @@
-window.trans = '/static/pong/js/translations/en.json';
-tmp_trans = null;
-
-function loadTranslations(newLang) {
-	const langFile = newLang === 'fr' ? '/static/pong/js/translations/fr.json' : 
-	newLang === 'es' ? '/static/pong/js/translations/es.json' :
-	'/static/pong/js/translations/en.json'; // Default to English if neither French nor Spanish
-	console.log(langFile);
-	tmp_trans = langFile;
-	console.log("tmp_trans: " + tmp_trans);
-	return fetch(langFile)
-	.then(response => response.json())
-	.then(trans => {
-		window.trans = trans;
-	})
-	.catch(error => {
-		console.error('Error fetching translations:', error);
-	})
-}
-
 function Home() {
-	if (tmp_trans == null)
-		loadTranslations(tmp_trans);
-    console.log("Translations:", window.trans);
+	// Need to use a tmp_trans to call loadTranslations only one at the beginning to avoid calling Home at the end of the file
+	// Can't use window.trans cause if I do, translations remain in english
+	if (window.tmp_trans == null)
+		loadTranslations(window.tmp_trans);
+
+	// Necessary cause the first translations don't load fast enough when first showing home page
 		const defaultMessage = "Welcome Home";
-   
-		// Use the fetched welcome message if available, otherwise fall back to the default message
 		const welcomeMessage = window.trans?.welcome || defaultMessage;
 
 		return `
 		<div>
 			<h1>${welcomeMessage}</h1>
 		</div>
-		<button class="btn btn-primary mb-3" onclick="loadHome('es')">Spanish</button>
-		<button class="btn btn-primary mb-3" onclick="loadHome('en')">English</button>
-		<button class="btn btn-primary mb-3" onclick="loadHome('fr')">French</button>`
+		<button class="btn btn-primary mb-3" onclick="changeLanguage('es')">Spanish</button>
+		<button class="btn btn-primary mb-3" onclick="changeLanguage('en')">English</button>
+		<button class="btn btn-primary mb-3" onclick="changeLanguage('fr')">French</button>`
 		;
 }
-
-function loadHome(newLang) {
-	// const langFile = newLang === 'fr' ? '/static/pong/js/translations/fr.json' : 
-	// newLang === 'es' ? '/static/pong/js/translations/es.json' :
-	// '/static/pong/js/translations/en.json'; // Default to English if neither French nor Spanish
-	// console.log("prefLang: " + newLang);
-	// console.log("langFile: " + langFile);
-	// window.trans = langFile;
-	// console.log("window.trans: " + window.trans);
-	loadTranslations(newLang).then(() => {
-		document.getElementById('app').innerHTML = Home();
-	});
-}
-
-// loadTranslations().then(() => {
-// 	document.getElementById('app').innerHTML = Home();
-// });
-
