5dce0843f0aba28543656df0173952004fc6cfa6
jfremond <jfremond@student.42.fr>
2024-08-09 00:44:29 +0200

    Menu bar is translated when changeLanguage is called

--- backend/project/pong/static/pong/js/loadTranslations.js
+++ backend/project/pong/static/pong/js/loadTranslations.js
@@ -1,11 +1,20 @@
 window.trans = '/static/pong/js/translations/en.json';
 
+// function setLanguageCookie(language) {
+//     var d = new Date();
+//     d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000)); // Cookie valid for 1 year
+//     var expires = "expires=" + d.toUTCString();
+//     document.cookie = "language=" + language + ";" + expires + ";path=/";
+// }
+
 function loadTranslations(newLang) {
 	let langFile = '/static/pong/js/translations/en.json';
 	if (newLang === 'fr')
 		langFile = '/static/pong/js/translations/fr.json';
 	else if (newLang === 'es')
 		langFile = '/static/pong/js/translations/es.json';
+	
+	// setLanguageCookie(newLang);
 	return fetch(langFile)
 	.then(response => response.json())
 	.then(trans => {
@@ -18,6 +27,7 @@ function loadTranslations(newLang) {
 
 function changeLanguage(newLang) {
 	loadTranslations(newLang).then(() => {
+		updateMenu();
 		document.getElementById('app').innerHTML = Home();
 	});
 }
