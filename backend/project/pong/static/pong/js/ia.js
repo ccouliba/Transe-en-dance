6542c2fd83721da5d3d8df428fd15826afde6025
Oren FREOUA <orfreoua@paul-f5Ar2s1.clusters.42paris.fr>
2024-08-13 12:14:38 +0200

    clear inter

--- backend/project/pong/static/pong/js/ia.js
+++ backend/project/pong/static/pong/js/ia.js
@@ -1,13 +1,15 @@
 let inGame = false;
+let intergame;
+let interia;
 
 function initializeGame() {
     // Création du canvas
     var canvas = document.createElement("canvas");
-    console.log("Canvas créé:", canvas);
+    //console.log("Canvas créé:", canvas);
     canvas.id = "myCanvas";
     canvas.width = 600;
     canvas.height = 300;
-    console.log("Dimensions du canvas:", canvas.width, canvas.height);
+    //console.log("Dimensions du canvas:", canvas.width, canvas.height);
 
     // Style pour centrer le canvas
     canvas.style.display = "block";
@@ -15,40 +17,40 @@ function initializeGame() {
     canvas.style.position = "relative";
     canvas.style.top = "50%";
     canvas.style.transform = "translateY(-50%)";
-    console.log("Style du canvas défini:", canvas.style);
+    //console.log("Style du canvas défini:", canvas.style);
 
     // Ajout du canvas au DOM
     document.body.appendChild(canvas);
 
     const ctx = canvas.getContext('2d');
-    console.log("Contexte 2D obtenu:", ctx);
+    //console.log("Contexte 2D obtenu:", ctx);
 
     // Définition des dimensions des paddles et initialisation des positions
     const paddleWidth = 10;
     const paddleHeight = canvas.height / 5;
-    console.log("Dimensions des paddles:", paddleWidth, paddleHeight);
+    //console.log("Dimensions des paddles:", paddleWidth, paddleHeight);
 
     const player = { x: 10, y: canvas.height / 2 - paddleHeight / 2, dy: 0 };
     const ai = { x: canvas.width - 20, y: canvas.height / 2 - paddleHeight / 2, dy: 0 };
     const ball = { x: canvas.width / 2, y: canvas.height / 2, dx: 2, dy: 2, radius: 7, speed: 2 };
-    console.log("Positions initiales:", "Player:", player, "AI:", ai, "Ball:", ball);
+    //console.log("Positions initiales:", "Player:", player, "AI:", ai, "Ball:", ball);
 
     // Couleurs
     const playerColor = '#00f';
     const aiColor = '#f00';
     const ballColor = '#0f0';
     const backgroundColor = '#000';
-    console.log("Couleurs définies:", "Player:", playerColor, "AI:", aiColor, "Ball:", ballColor, "Background:", backgroundColor);
+    //console.log("Couleurs définies:", "Player:", playerColor, "AI:", aiColor, "Ball:", ballColor, "Background:", backgroundColor);
 
     // Variables de score
     let playerScore = 0;
     let aiScore = 0;
     const maxScore = 5;
-    console.log("Scores initiaux:", "Player:", playerScore, "AI:", aiScore);
+    //console.log("Scores initiaux:", "Player:", playerScore, "AI:", aiScore);
 
     // Gestion des événements de pression de touche
     document.addEventListener('keydown', function(e) {
-        console.log("Touche pressée:", e.key);
+        //console.log("Touche pressée:", e.key);
         if (e.key === 'ArrowUp') player.dy = -8;
         if (e.key === 'ArrowDown') player.dy = 8;
         if (e.key === '+') ai.dy = -8;
@@ -57,7 +59,7 @@ function initializeGame() {
     });
 
     document.addEventListener('keyup', function(e) {
-        console.log("Touche relâchée:", e.key);
+        //console.log("Touche relâchée:", e.key);
         if (e.key === 'ArrowUp' || e.key === 'ArrowDown') player.dy = 0;
         if (e.key === '+' || e.key === '-') ai.dy = 0;
     });
@@ -65,14 +67,14 @@ function initializeGame() {
     function simulateKey(key, isKeyDown = true) {
         const eventType = isKeyDown ? 'keydown' : 'keyup';
         const event = new KeyboardEvent(eventType, { key });
-        console.log("Simulation de touche:", eventType, key);
+        //console.log("Simulation de touche:", eventType, key);
         document.dispatchEvent(event);
     }
 
     function update() {
         if (gameOver) return;
 
-        console.log("Mise à jour des positions:", "Player Y:", player.y, "AI Y:", ai.y, "Ball:", ball);
+        //console.log("Mise à jour des positions:", "Player Y:", player.y, "AI Y:", ai.y, "Ball:", ball);
 
         player.y += player.dy;
         if (player.y < 0) player.y = 0;
@@ -94,7 +96,7 @@ function initializeGame() {
         }
 
         if (checkCollision(ball, player)) {
-            console.log("Collision avec le joueur");
+            //console.log("Collision avec le joueur");
             ball.x = player.x + paddleWidth + ball.radius;
             ball.dx *= -1;
             ball.dy = (ball.y - (player.y + paddleHeight / 2)) * 0.2;
@@ -102,7 +104,7 @@ function initializeGame() {
         }
 
         if (checkCollision(ball, ai)) {
-            console.log("Collision avec l'IA");
+            //console.log("Collision avec l'IA");
             ball.x = ai.x - ball.radius;
             ball.dx *= -1;
             ball.dy = (ball.y - (ai.y + paddleHeight / 2)) * 0.2;
@@ -111,21 +113,21 @@ function initializeGame() {
 
         if (ball.x + ball.radius > canvas.width) {
             playerScore++;
-            console.log("But! Score du joueur:", playerScore);
+            //console.log("But! Score du joueur:", playerScore);
             if (playerScore >= maxScore) {
                 gameOver = true;
                 winner = 'Player';
-                console.log("Le joueur gagne!");
+                //console.log("Le joueur gagne!");
             } else {
                 resetBall();
             }
         } else if (ball.x - ball.radius < 0) {
             aiScore++;
-            console.log("But! Score de l'IA:", aiScore);
+            //console.log("But! Score de l'IA:", aiScore);
             if (aiScore >= maxScore) {
                 gameOver = true;
                 winner = 'AI';
-                console.log("L'IA gagne!");
+                //console.log("L'IA gagne!");
             } else {
                 resetBall();
             }
@@ -139,7 +141,7 @@ function initializeGame() {
     function animateCenteringAiPaddle(pos) {
         if (!animatingAiPaddle) {
             animatingAiPaddle = true;
-            console.log("Centrage du paddle de l'IA vers la position:", pos);
+            //console.log("Centrage du paddle de l'IA vers la position:", pos);
 
             let targetY;
             if (pos === 0)
@@ -154,7 +156,7 @@ function initializeGame() {
                 targetY = canvas.height / 2 - paddleHeight / 2 + (paddleHeight * 2);
 
             let startY = ai.y;
-            console.log("Début de l'animation du paddle IA de", startY, "vers", targetY);
+            //console.log("Début de l'animation du paddle IA de", startY, "vers", targetY);
 
             function animateStep() {
                 if (startY <= targetY) {
@@ -162,15 +164,16 @@ function initializeGame() {
                     if (ai.y >= targetY) {
                         simulateKey('-', false);
                         animatingAiPaddle = false;
-                        console.log("Fin de l'animation du paddle IA.");
+                        //console.log("Fin de l'animation du paddle IA.");
                         return;
                     }
                 } else {
                     simulateKey('+');
                     if (ai.y <= targetY) {
+                        setInterval(updateAI, 1000);
                         simulateKey('+', false);
                         animatingAiPaddle = false;
-                        console.log("Fin de l'animation du paddle IA.");
+                        //console.log("Fin de l'animation du paddle IA.");
                         return;
                     }
                 }
@@ -190,7 +193,7 @@ function initializeGame() {
     }
 
     function updateAI() {
-        console.log("Mise à jour de l'IA");
+        //console.log("Mise à jour de l'IA");
         if (ball.dx < 0) 
             centerAiPaddle(2);
         else if (ball.x < canvas.width / 2 + ((canvas.width / 2) / 3))
@@ -199,7 +202,7 @@ function initializeGame() {
             centerAiPaddle(predictBallY());
     }
 
-    setInterval(updateAI, 1000);
+    interia = setInterval(updateAI, 1000);
 
     function checkCollision(ball, paddle) {
         return ball.x - ball.radius < paddle.x + paddleWidth &&
@@ -214,7 +217,7 @@ function initializeGame() {
         ball.dx = 2 * (Math.random() > 0.5 ? 1 : -1);
         ball.dy = 2 * (Math.random() > 0.5 ? 1 : -1);
         ball.speed = 2;
-        console.log("Balle réinitialisée:", ball);
+        //console.log("Balle réinitialisée:", ball);
     }
 
     function draw() {
@@ -262,7 +265,7 @@ function initializeGame() {
     
     function getBallSection(ballY) {
         const sectionHeight = canvas.height / 5;
-        console.log("Section de la balle:", ballY, "Section:", Math.floor(ballY / sectionHeight));
+        //console.log("Section de la balle:", ballY, "Section:", Math.floor(ballY / sectionHeight));
         if (ballY < sectionHeight) return 0;
         if (ballY < sectionHeight * 2) return 1;
         if (ballY < sectionHeight * 3) return 2;
@@ -279,7 +282,7 @@ function initializeGame() {
                 tempBall.dy *= -1;
             }
         }
-        console.log("Prédiction de la position Y de la balle:", tempBall.y);
+        //console.log("Prédiction de la position Y de la balle:", tempBall.y);
         return getBallSection(tempBall.y);
     }
 
@@ -289,16 +292,19 @@ function initializeGame() {
         gameOver = false;
         winner = '';
         resetBall();
-        console.log("Jeu réinitialisé.");
+        //console.log("Jeu réinitialisé.");
     }
 
-    setInterval(gameLoop, 1000 / 60);
+    intergame = setInterval(gameLoop, 1000 / 60);
 }
 
 function toggleCanvas() {
+    
+    clearInterval(intergame);
+    //console.log(intergame);
     inGame = true;
-    console.log("in game");
-    console.log(inGame);
+    //console.log("in game");
+    //console.log(inGame);
     var canvas = document.getElementById('myCanvas');
     if (canvas) {
         resetGame();
@@ -308,26 +314,17 @@ function toggleCanvas() {
 }
 
 function hideCanvas() {
+    if (interia)
+        clearInterval(interia);
+    if (intergame)
+        clearInterval(intergame);
     inGame = false;
-    console.log("out game");
-    console.log(inGame);
+    //console.log("out game");
+    //console.log(inGame);
     var canvas = document.getElementById('myCanvas');
     if (canvas) {
         document.body.removeChild(canvas);
     }
-}
-
-function handleKeyDown(e) {
-    if (e.key === 'ArrowUp') player.dy = -8;
-    if (e.key === 'ArrowDown') player.dy = 8;
-    if (e.key === '+') ai.dy = -8;
-    if (e.key === '-') ai.dy = 8;
-    if (e.key === 'Enter' && gameOver) resetGame();
-}
-
-function handleKeyUp(e) {
-    if (e.key === 'ArrowUp' || e.key === 'ArrowDown') player.dy = 0;
-    if (e.key === '+' || e.key === '-') ai.dy = 0;
 }
 
 function IA() {
