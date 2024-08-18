1dc9b5c0b9af671736c67d138ce51d9f793e4185
naxchin <nathaliexiaodan.chin@gmail.com>
2024-08-18 22:04:50 +0200

    removing dead code from home.js and base.html

--- Transcendence/project/back/static/pong/js/home.js
+++ Transcendence/project/back/static/pong/js/home.js
@@ -1,14 +1,3 @@
-
-// function Home() {
-// 	return `
-// 		<div class="container">
-// 		<div class="home">
-// 			<h1 style="text-align: center;" id="homeWelcomeTxt">${window.trans.welcome}</h1>
-// 		</div>
-// 	</div>`;
-// }
-
-
 function Home() {
 	function TutorialMenu() {
 		return `
