d9b17774bf79447f5e30d73bf01a679ae59e5817
Riyettes <ccouliba@student.42.fr>
2024-08-20 23:25:43 +0200

    this is a nice commit so i did some corrections and now it's time to push

--- Transcendence/project/server/settings.py
+++ Transcendence/project/server/settings.py
@@ -72,7 +72,6 @@ INSTALLED_APPS = [
 	'django.contrib.staticfiles',
 	'rest_framework',
 	'rest_framework.authtoken',
-    'django_elasticsearch_dsl',
 ]
 
 MIDDLEWARE = [
@@ -243,7 +242,7 @@ LOGGING = {
             'datefmt': '%Y/%m/%d %H:%M:%S',
         },
         'simple': {
-            'format': '%(name)s %(message)s',
+            'format': '%(name)s %(funcName)s %(message)s',
         },
     },
 
